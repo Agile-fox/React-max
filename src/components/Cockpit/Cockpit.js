9002337e9857370697b542de9d302b1b52c36b87
unknown <jakub@wirtualnafotografia.pl>
2020-01-04 19:48:50 +0100

    Changing structure

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -19,7 +19,7 @@ const cockpit = (props) => {
 
     return(
         <div className={classes.Cockpit}>
-            <h1> Hi, I'm a react app from Jakub!</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>It works!</p>
             <button 
             className={btnClass}
