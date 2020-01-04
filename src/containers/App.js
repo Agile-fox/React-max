c057fb177faf2f46b7168b9d7d66ea4a6eb5dd8c
unknown <jakub@wirtualnafotografia.pl>
2020-01-04 20:07:34 +0100

    Adding component lifecycles

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1a', name: 'Jakub', age: 23 },
@@ -15,10 +20,19 @@ class App extends Component {
     showPerson: false
   }
 
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState( { persons: persons});
+  }
+
+  componentDidMount() {
+    console.log('[App.js] ComponentDidMount');
   }
 
   nameChangedHandler = (event, id) => {
@@ -43,7 +57,7 @@ class App extends Component {
   }
 
   render(){
-   
+   console.log('[App.js] render');
     let persons = null;
 
 
