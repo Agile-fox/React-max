436030417f00ca1b8df5344e8aa89bda5555787d
unknown <jakub@wirtualnafotografia.pl>
2020-01-08 14:06:45 +0100

    Adding high order component folder and usage of react fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary'
 
 class Person extends Component { 
     render() {
         console.log('[Person.js] rendering...');
         return (
-        <div className={classes.Person}>
+            <Fragment>
                  <p onClick={this.props.click}>I'm a {this.props.name}! And i have {this.props.age} years old</p>
-                 <p>{this.props.children}</p>
+                 <p key="i2">{this.props.children}</p>
                  <input 
+                 key="i3"
                  type="text"
                  onChange={this.props.changed}
                  value={this.props.name}/>
-        </div>
+            </Fragment>
         );
      }
 }
